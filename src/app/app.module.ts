b8532eb6a5f59278e94187e89041285a00ed7765
Ryan Efendy <ryanefendy95@gmail.com>
2018-10-20 23:13:31 -0700

    FEAT: add welcome, home, grass component :tada::tada::tada:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
+import { GrassComponent } from './grass/grass.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HomeComponent,
+    GrassComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/grass/grass.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grass',
+  templateUrl: './grass.component.html',
+  styleUrls: ['./grass.component.css']
+})
+export class GrassComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
